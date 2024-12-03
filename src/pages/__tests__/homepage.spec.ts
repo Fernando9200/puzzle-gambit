c5d0b264eb9bd92df067d7dacf576a35bba11a35
Fernando9200 <fernando91.sosa@hotmail.com>
2024-12-03 16:30:35 -0300

    Removed unused code

--- src/pages/__tests__/homepage.spec.ts
+++
@@ -1,9 +0,0 @@
-import Homepage from '../homepage.vue'
-import { fireEvent } from '@testing-library/vue'
-import { renderWithVuetify } from '@/../test/helpers'
-
-describe('homepage', () => {
-  it('Notify correctly', async () => {
-    const { getByText, getByLabelText } = renderWithVuetify(Homepage)
-  })
-})
